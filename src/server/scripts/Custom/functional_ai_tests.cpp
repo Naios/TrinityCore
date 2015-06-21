cbe2929f9787756ffc22e5521e27d70d79838b40
Naios <naios-dev@live.de>
2015-06-21 19:03:31 +0200

    adapt to the latest changes

--- src/server/scripts/Custom/functional_ai_tests.cpp
+++ src/server/scripts/Custom/functional_ai_tests.cpp
@@ -20,20 +20,6 @@
 
 #include "TaskScheduler.h"
 
-///////////////////
-// Not finished, early prototypes (maybe 1 single SimpleLoader can be used with std::enable_if).
-template <class _Script>
-class SimpleCreatureScriptLoader : public CreatureScript
-{
-public:
-    SimpleCreatureScriptLoader(char const* scriptname) : CreatureScript(scriptname) { }
-
-    CreatureAI* GetAI(Creature* creature) const override
-    {
-        return new _Script(creature);
-    }
-};
-
 // This script shows how to use the FunctionScheduler as EventMap replacement.
 // Embedded inside a CreatureScript
 class npc_legacy_simple_scheduler_test : public CreatureScript
@@ -45,17 +31,34 @@ public:
     {
         TaskScheduler scheduler;
 
-        npc_legacy_simple_scheduler_testAI(Creature* creature) : CreatureAI(creature)
+        bool outro;
+
+        npc_legacy_simple_scheduler_testAI(Creature* creature)
+            : CreatureAI(creature)
         {
+            scheduler.SetValidator([this]
+            {
+                return !me->HasUnitState(UNIT_STATE_CASTING);
+            });
         }
 
         void Reset() override
         {
+            // Cancel all tasks
             scheduler.CancelAll();
+
+            outro = false;
+
+            me->SetReactState(REACT_AGGRESSIVE);
         }
 
         void EnterCombat(Unit* /*who*/) override
         {
+            me->Yell("Mh who is that...", LANG_UNIVERSAL);
+
+            // Schedule a simple task that gets executed once in 5 seconds.
+            // As time unit std::chrono::duration is used which offers typedef for common used timeunits.
+            // No magic milliseconds anymore!
             scheduler.Schedule(Seconds(5), [this](TaskContext /*context*/)
             {
                 me->Yell("After 5s", LANG_UNIVERSAL);
@@ -64,12 +67,72 @@ public:
 
         void UpdateAI(uint32 diff) override
         {
-            if (!UpdateVictim())
-                return;
+            if (!outro)
+                if (!UpdateVictim())
+                    return;
 
-            scheduler.Update(diff);
-            
-            DoMeleeAttackIfReady();
+            // Update the scheduler with an offset, world update tick is in ms.
+            scheduler.Update(diff, [this]
+            {
+                // Use melee attacks
+                if (!outro)
+                    DoMeleeAttackIfReady();
+            });
+        }
+
+        void DamageTaken(Unit* /*attacker*/, uint32& damage)
+        {
+            if (!outro && (damage >= me->GetHealth()))
+            {
+                damage = 0;
+                outro = true;
+
+                me->SetReactState(REACT_PASSIVE);
+
+                me->Yell("Outro....!", LANG_UNIVERSAL);
+
+                // A simple outro event is scheduled here.
+                // TaskContext offers several methods to repeat the event (same time, constant time/ random time).
+                //
+                // The task also provides a repeat counter which repeats
+                // how often the task was repeated (best to use with dialogs or events).
+                scheduler.Schedule(Seconds(3), [this](TaskContext context)
+                {
+                    switch (context.GetRepeatCounter())
+                    {
+                        case 0:
+                            me->Yell("O shit!", LANG_UNIVERSAL);
+                            context.Repeat(Seconds(3));
+                            return;
+                        case 1:
+                            me->Yell("I see it was a mistake to fight you.", LANG_UNIVERSAL);
+                            break;
+                        case 2:
+                            me->Yell("There is no loot you can get today, sorry!", LANG_UNIVERSAL);
+                            break;
+                        case 3:
+                            me->Yell("See YA!", LANG_UNIVERSAL);
+                            break;
+                        default:
+                            me->DisappearAndDie();
+                            return;
+                    }
+
+                    /////////
+                    // Repeat the event with...
+
+                    // ... the same time again
+                    // context->Repeat();
+
+                    // ... a constant time
+                    // context->Repeat(Seconds(4));
+
+                    // ... a random time between min and max
+                    context.Repeat(Seconds(4), Seconds(6));
+                });
+            }
+            else if (outro)
+                damage = 0;
         }
     };
 
@@ -79,113 +142,7 @@ public:
     }
 };
 
-struct npc_simple_scheduler_test : public CreatureAI
-{
-    TaskScheduler scheduler;
-
-    bool outro;
-
-    npc_simple_scheduler_test(Creature* creature) : CreatureAI(creature)
-    {
-    }
-
-    void Reset() override
-    {
-        // Cancel all tasks
-        scheduler.CancelAll();
-
-        outro = false;
-
-        me->SetReactState(REACT_AGGRESSIVE);
-    }
-
-    void EnterCombat(Unit* /*who*/) override
-    {
-        me->Yell("Mh who is that...", LANG_UNIVERSAL);
-
-        // Schedule a simple task that gets executed once in 5 seconds.
-        // As time unit std::chrono::duration is used which offers typedef for common used timeunits.
-        // No magic milliseconds anymore!
-        scheduler.Schedule(Seconds(5), [this](TaskContext /*context*/)
-        {
-            me->Yell("After 5s", LANG_UNIVERSAL);
-        });
-    }
-
-    void UpdateAI(uint32 diff) override
-    {
-        if (!outro)
-            if (!UpdateVictim())
-                return;
-
-        // Update the scheduler with an offset, world update tick is in ms.
-        scheduler.Update(diff);
-
-        if (!outro)
-            DoMeleeAttackIfReady();
-    }
-
-    void DamageTaken(Unit* /*attacker*/, uint32& damage)
-    {
-        if (!outro && (damage >= me->GetHealth()))
-        {
-            damage = 0;
-            outro = true;
-
-            me->SetReactState(REACT_PASSIVE);
-
-            me->Yell("Outro....!", LANG_UNIVERSAL);
-
-            // A simple outro event is scheduled here.
-            // TaskContext offers several methods to repeat the event (same time, constant time/ random time).
-            //
-            // The task also provides a repeat counter which repeats
-            // how often the task was repeated (best to use with dialogs or events).
-            scheduler.Schedule(Seconds(3), [this](TaskContext context)
-            {
-                switch (context->GetRepeatCounter())
-                {
-                    case 0:
-                        me->Yell("O shit!", LANG_UNIVERSAL);
-                        context->Repeat(Seconds(3));
-                        return;
-                    case 1:
-                        me->Yell("I see it was a mistake to fight you.", LANG_UNIVERSAL);
-                        break;
-                    case 2:
-                        me->Yell("There is no loot you can get today, sorry!", LANG_UNIVERSAL);
-                        break;
-                    case 3:
-                        me->Yell("See YA!", LANG_UNIVERSAL);
-                        break;
-                    default:
-                        me->DisappearAndDie();
-                        return;
-                }
-
-                /////////
-                // Repeat the event with...
-
-                // ... the same time again
-                // context->Repeat();
-
-                // ... a constant time
-                // context->Repeat(Seconds(4));
-
-                // ... a random time between min and max
-                context->Repeat(Seconds(4), Seconds(6));
-            });
-        }
-        else if (outro)
-            damage = 0;
-    }
-};
-
 void AddSC_functional_ai_tests()
 {
     new npc_legacy_simple_scheduler_test();
-
-    // In nearly every script CreatureAI is wrapped inside a CreatureScript, this approach removes a lot of duplicated code.
-    // SimpleLoaders wrap its AI into the corresponding loading script.
-    new SimpleCreatureScriptLoader<npc_simple_scheduler_test>("npc_simple_scheduler_test");
 }
