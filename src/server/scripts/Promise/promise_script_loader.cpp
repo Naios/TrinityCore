b9521e71200fdb72e2d3e4f2bbaf46c17d8f5a53
Naios <naios-dev@live.de>
2016-07-09 01:04:34 +0200

    more thoughts

--- src/server/scripts/Promise/promise_script_loader.cpp
+++ src/server/scripts/Promise/promise_script_loader.cpp
@@ -15,9 +15,116 @@
  * with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "ScriptedCreature.h"
+
+// Testing
 #include "World.h"
 #include "Duration.h"
 #include "Spell.h"
+
+class AsyncAIBase : public ScriptedAI
+{
+public:
+    explicit AsyncAIBase(Creature* creature) : ScriptedAI(creature) { }
+    virtual ~AsyncAIBase() { }
+
+    void Reset() override
+    {
+    }
+
+    void EnterCombat(Unit*) override
+    {
+    }
+};
+
+class RecurringEvent
+{
+public:
+    RecurringEvent() { }
+
+    RecurringEvent operator| (RecurringEvent event)
+    {
+        return event;
+    }
+};
+
+template<typename... Args>
+class RecurringEventEmitter
+{
+    RecurringEvent next;
+
+public:
+    RecurringEvent operator| (RecurringEvent event)
+    {
+        return event;
+    }
+
+    RecurringEvent AsEvent()
+    {
+        return { };
+    }
+};
+
+class AsyncAI
+{
+public:
+    explicit AsyncAI(Creature* creature) { }
+    virtual ~AsyncAI() { }
+
+    virtual void SetUp() = 0;
+
+    RecurringEvent CastSpell(uint32 spellId) const
+    {
+        return { };
+    }
+
+    RecurringEvent MoveTo(Position pos) const
+    {
+        return { };
+    }
+
+    RecurringEvent MoveToHome() const
+    {
+        return { };
+    }
+
+    RecurringEvent In(Milliseconds) const
+    {
+        return { };
+    }
+
+    RecurringEvent Talk(std::string) const
+    {
+        return { };
+    }
+
+protected:
+    RecurringEventEmitter<Unit*> onEnterCombat;
+};
+
+class MyAsyncAI : public AsyncAI
+{
+public:
+    explicit MyAsyncAI(Creature* creature)
+        : AsyncAI(creature)  { }
+
+    RecurringEventEmitter<> startSpeechEvent;
+
+    void SetUp() final override
+    {
+        onEnterCombat
+            | CastSpell(12822)
+            | MoveTo({ 1, 1, 1, 0 })
+            | In(Seconds(10))
+            | MoveToHome()
+            | startSpeechEvent.AsEvent();
+
+        startSpeechEvent
+            | Talk("Hey")
+            | Talk("2. sentence")
+            | Talk("3. sentence");
+    }
+};
 
 namespace FutureScripting {
     template<typename T = void>
@@ -98,6 +205,7 @@ struct FutureAI
                              std::chrono::duration<Rep2, Period2>) {
         return { };
     }
+
 
     void Test()
     {
