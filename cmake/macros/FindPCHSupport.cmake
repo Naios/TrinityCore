46c612244a7a6c0f7c64875e2349e0c92bae76c1
Naios <naios-dev@live.de>
2018-01-25 21:04:43 +0100

    Build: Add dummy files to PCH (cotirized) projects * This is an easy solution to our known rebuild single file issue,   maybe I will solve this in a different way in the future. * Closes #21104

--- cmake/macros/FindPCHSupport.cmake
+++ cmake/macros/FindPCHSupport.cmake
@@ -10,7 +10,7 @@ endif()
 
 include(cotire)
 
-function(ADD_CXX_PCH TARGET_NAME_LIST PCH_HEADER)
+macro(ADD_CXX_PCH TARGET_NAME_LIST PCH_HEADER)
   # Use the header for every target
   foreach(TARGET_NAME ${TARGET_NAME_LIST})
     # Disable unity builds
@@ -21,7 +21,32 @@ function(ADD_CXX_PCH TARGET_NAME_LIST PCH_HEADER)
 
     # Workaround for cotire bug: https://github.com/sakra/cotire/issues/138
     set_property(TARGET ${TARGET_NAME} PROPERTY CXX_STANDARD 14)
+
+    # This is a workaround for #21104 (it may be removed in the future as
+    # soon as cotire provides a fix for this).
+    # We add a dummy file as first source file in the built project where the
+    # corresponding PCH files get added to. This is equal to the way we've
+    # done this in the previous PCH implementation.
+    #
+    # Create the filename for the PCH source ("CommonPCH.h" -> "CommonPCH.cpp")
+    get_filename_component(PCH_SOURCE ${PCH_HEADER} NAME_WE)
+    set(PCH_SOURCE "${CMAKE_CURRENT_BINARY_DIR}/gen_pch/${TARGET_NAME}/${PCH_SOURCE}.cpp")
+    #
+    # Just touch the file
+    if(NOT EXISTS "${PCH_SOURCE}")
+      file(WRITE ${PCH_SOURCE} "")
+    endif()
+    #
+    # Add the corresponding PCH source file as the first
+    get_target_property(ALL_TARGET_SOURCES ${TARGET_NAME} SOURCES)
+    # Insert the PCH source file in front (important),
+    # since this is how cotire selects the source file to attach flags to
+    list(INSERT ALL_TARGET_SOURCES 0 ${PCH_SOURCE})
+
+    # Re-set the project sources
+    set_target_properties(${TARGET_NAME}
+                          PROPERTIES SOURCES "${ALL_TARGET_SOURCES}")
   endforeach()
 
   cotire(${TARGET_NAME_LIST})
-endfunction(ADD_CXX_PCH)
+endmacro(ADD_CXX_PCH)
